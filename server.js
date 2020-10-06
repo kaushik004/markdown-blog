6a0306617836b13f93678fee844e6d59a766424b
kaushik <kaushik@gmail.com>
2020-10-06 12:52:36 +0530

    added route file.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
+
+app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
     res.render('index');
