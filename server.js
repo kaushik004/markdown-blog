b0fba27f62e388148612d30bbaf4e46e4cf836c1
kaushik <kaushik@gmail.com>
2020-10-07 09:48:41 +0530

    created model and added new route

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
